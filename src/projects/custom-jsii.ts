422e6f4a7c1864c31b214e11bc881bbac17934b9
Vlad Cos <vcosvic@gmail.com>
2023-08-24 16:05:01 +0300

    WIP

--- src/projects/custom-jsii.ts
+++ src/projects/custom-jsii.ts
@@ -5,7 +5,7 @@ import { getSharedOptions } from '../shared'
 export class CustomJsii extends RootProject {
   constructor(options: any = {}) {
     super(getSharedOptions(options))
-    this.addDeps('@vladcos/projen-base@../projen-base/')
-    this.addPeerDeps('@vladcos/projen-base@../projen-base/')
+    this.addDeps('@vladcos/projen-base')
+    this.addPeerDeps('@vladcos/projen-base')
   }
 }
