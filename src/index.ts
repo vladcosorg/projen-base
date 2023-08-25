cc0aa7a4512d3bda5fb5d50dea0d01b94374d42e
Vlad Cos <vcosvic@gmail.com>
2023-08-25 16:30:33 +0300

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Monorepo } from './projects/monorepo'
 export { MonorepoPackage } from './projects/monorepo-package'
 
+export * from './projects/github-action/github-action'
 export * from './projects/typescript'
 export * from './projects/root-project'
 export * from './projects/custom-jsii'
