133af9c9279a32038d16f117cc7db10b5b5f4df3
Vlad Cos <vcosvic@gmail.com>
2023-08-27 22:16:52 +0300

    WIP

--- src/projects/custom-jsii.ts
+++ src/projects/custom-jsii.ts
@@ -3,7 +3,6 @@ import { RootProject } from './root-project'
 export class CustomJsii extends RootProject {
   constructor(options: any = {}) {
     super(options)
-    this.addDeps('@vladcos/projen-base')
-    this.addPeerDeps('@vladcos/projen-base')
+    this.addDevDeps('@vladcos/projen-base')
   }
 }
