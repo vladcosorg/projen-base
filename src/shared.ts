c1540f7bb82f95c1ff29a45824ce6ab37b4c2a4d
Vlad Cos <vcosvic@gmail.com>
2023-09-07 13:40:04 +0300

    chrome: Upgraded peer deps

--- src/shared.ts
+++ src/shared.ts
@@ -64,11 +64,10 @@ export function getSharedOptions<
 
 export function preSynthesize(project: JsiiProject | TypeScriptProject): void {
   project.testTask.reset()
-  const version = 'projen@0.72.19'
-  project.addDevDeps(version)
+  project.addDevDeps('projen@0.73.9')
 
   if (!('packemon' in project)) {
-    project.addPeerDeps(version)
+    project.addPeerDeps('projen@0.73.9')
   }
 
   project.npmrc.addConfig('install-links', 'false')
