e268c604a1042cacbcb32e68dce6909ef2f4b899
Vlad Cos <vcosvic@gmail.com>
2023-09-07 19:54:17 +0300

    WIP

--- src/projects/custom-jsii.ts
+++ src/projects/custom-jsii.ts
@@ -3,7 +3,9 @@ import { RootProject } from './root-project'
 export class CustomJsii extends RootProject {
   constructor(options: any = {}) {
     super(options)
-    this.addDevDeps('@vladcos/projen-base@^0.0')
-    this.addPeerDeps('@vladcos/projen-base@^0.0')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const version = require('../../package.json').version
+    this.addDevDeps(`@vladcos/projen-base@${version}`)
+    this.addPeerDeps(`@vladcos/projen-base@${version}`)
   }
 }
